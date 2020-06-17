e2028baa08edce6bb33548b7b6b22bbf4dfe1fd2
darkdarcool <66882633+darkdarcool@users.noreply.github.com>
2020-06-17 14:38:31 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -18,7 +18,7 @@ let bird = sprites.create(img`
     . c d d d d d d 5 5 5 5 5 d b .
     . . c b d d d d d 5 5 5 b b . .
     . . . c c c c c c c c b b . . .
-`)
+`, SpriteKind.Player)
 
 bird.ay = 200
 
@@ -36,3 +36,163 @@ game.onUpdate(function () {
     }    
 
 })
+
+info.setScore(0)
+
+game.onUpdateInterval(1000, function () {
+    info.changeScoreBy(1)
+})
+
+
+function spawnTopTree(){
+    let obsticleTop = sprites.create(img`
+        . . . . . 6 f c e e e e e e e e e e 6 . . . . .
+        . . . . 6 7 7 6 e e e e e e e e e 6 7 6 . . . .
+        . . . 6 7 7 7 6 6 6 e e e e 6 6 6 6 7 7 6 . . .
+        . . 6 7 7 6 8 e 6 7 7 6 6 7 7 7 6 6 7 7 7 6 . .
+        . . . 6 6 8 e e 7 7 6 8 8 6 7 7 8 8 6 6 6 . . .
+        . . . . . . e e 7 7 e e e e 7 7 e c e e . . . .
+        . . . . . . e e 6 e e e e e e 6 e e f . . . . .
+        . . . . . . e e e e e e e e e e e e f . . . . .
+        . . . . . . e e e e e e e e e e e e f . . . . .
+        . . . . . . e e e e e e e e e e e c f . . . . .
+        . . . . . . c e e e e e e e e e e c f . . . . .
+        . . . . . . c e e e e e e e e e e f f . . . . .
+        . . . . . . f e e e e e e e e e e f e . . . . .
+        . . . . . 6 f e e e e e e e e e e f 6 . . . . .
+        . . . . 6 7 7 6 e e e e e e e e e 6 7 6 . . . .
+        . . . 6 7 7 7 6 6 6 e e e e 6 6 6 7 7 7 6 . . .
+        . . 6 7 7 6 e e 6 7 7 7 7 7 7 7 6 6 7 7 7 6 . .
+        . . . 6 6 8 e e 7 7 6 8 8 6 7 7 8 8 6 6 6 . . .
+        . . . . . . e e 7 7 e e e e 6 7 e e e e . . . .
+        . . . . . . e e 6 e e e e e e 6 c e f . . . . .
+        . . . . . . e e e e e e e e e e e e f . . . . .
+        . . . . . . e e e e e e e e e e e e f . . . . .
+        . . . . . . e e e e e e e e e e e c f . . . . .
+        . . . . . . e e e e e e e e e e e c f . . . . .
+        . . . . . . e e e e e e e e e e e f f . . . . .
+        . . . . . . f e e e e e e e e e e f e . . . . .
+        . . . . . 6 f e e e e e e e e e e f 6 . . . . .
+        . . . . 6 7 7 6 e e e e e e e e e 6 7 6 . . . .
+        . . . 6 7 7 7 6 6 6 e e e e 6 6 6 7 7 7 6 . . .
+        . . 6 7 7 6 e e 6 7 7 7 7 7 7 7 6 6 7 7 7 6 . .
+        . . . 6 6 8 e e 7 7 6 8 8 6 7 7 8 8 6 6 6 . . .
+        . . . . . . e e 7 7 e e e e 6 7 e e . . . . . .
+        . . . . . . e e 6 e e e e e e 6 c e . . . . . .
+        . . . . . . e e f e e e e e e e c e . . . . . .
+        . . . . . . e e c e e e e e e e c e . . . . . .
+        . . . . . . e e c e e e e e e e f e . . . . . .
+        . . . . . . e e c e e e e e e e f e . . . . . .
+        . . . . . . e e e e e e e e e e f e . . . . . .
+        . . . . . . e e e e e e e e e e c e . . . . . .
+        . . . . . 6 e e e e e e e e e e c e 6 . . . . .
+        . . . . 6 7 7 6 e e e e e e e e e 6 7 6 . . . .
+        . . . 6 7 7 6 6 6 6 e e e e 6 7 6 6 7 7 6 . . .
+        . . 6 7 7 6 e e 7 7 7 7 7 7 7 7 6 6 7 7 7 6 . .
+        . . . 6 6 8 c e 7 7 6 8 8 6 7 7 8 8 6 6 6 . . .
+        . . . . . . c e 7 7 e e e e 6 7 e e . . . . . .
+        . . . . . . e e e e e e e e e e e e . . . . . .
+        . . . . . . e e e e e e e e e e e e . . . . . .
+        . . . . . . e e e e e e e e e e e e . . . . . .
+        . . . . . . e e e e e e e e e e e e . . . . . .
+        . . . . . . e e e e e e e e e e e e . . . . . .
+        . . . . . . e e e e e e e e e e e e . . . . . .
+        . . . . . . e e e e e e e e e e e e . . . . . .
+        . . . . . . b e e e e e e e e e e b . . . . . .
+        . . . . . . . b e e e e e e e e b . . . . . . .
+        . . . . . . . . b e e e e e e b . . . . . . . .
+        . . . . . . . . . . . . . . . . . . . . . . . .
+    `, SpriteKind.Enemy)
+
+    obsticleTop.setPosition(scene.screenWidth(), 25)
+    obsticleTop.vx = -25
+}
+
+
+
+function spawnBottemTree(){
+    let obsticleBottom = sprites.create(img`
+        . . . . . . . . . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . . . . . . . . . .
+        . . . . . . . . . . b b b b . . . . . . . . . .
+        . . . . . . . . b b d d d d b b . . . . . . . .
+        . . . . . . . b d d b b b b d d b . . . . . . .
+        . . . . . . b d b b d d d d b b d b . . . . . .
+        . . . . . b d b b d b b b b d b b d b . . . . .
+        . . . . . b d b d b d d d d b d b d b . . . . .
+        . . . . . c d b b d b b b b d b b d c . . . . .
+        . . . . . c b d b b d d d d b b d b c . . . . .
+        . . . . . e f b d d b b b b d d b c e . . . . .
+        . . . . . e e f f b d d d d b c c e e . . . . .
+        . . . . . e e e e f f c c c c e e e . . . . . .
+        . . . . . c e e e e e e e e e e e e . . . . . .
+        . . . . . c e e e e e e e e e e e e . . . . . .
+        . . . . . f e e e e e e e e e e e e . . . . . .
+        . . . . . c c e e e e e e e e e e e . . . . . .
+        . . . . . . f e e e e e e e e e e e . . . . . .
+        . . . . . 6 f c e e e e e e e e e e 6 . . . . .
+        . . . . 6 7 7 6 e e e e e e e e e 6 7 6 . . . .
+        . . . 6 7 7 7 6 6 6 e e e e 6 6 6 6 7 7 6 . . .
+        . . 6 7 7 6 8 e 6 7 7 6 6 7 7 7 6 6 7 7 7 6 . .
+        . . . 6 6 8 e e 7 7 6 8 8 6 7 7 8 8 6 6 6 . . .
+        . . . . . . e e 7 7 e e e e 7 7 e c e e . . . .
+        . . . . . . e e 6 e e e e e e 6 e e f . . . . .
+        . . . . . . e e e e e e e e e e e e f . . . . .
+        . . . . . . e e e e e e e e e e e e f . . . . .
+        . . . . . . e e e e e e e e e e e c f . . . . .
+        . . . . . . c e e e e e e e e e e c f . . . . .
+        . . . . . . c e e e e e e e e e e f f . . . . .
+        . . . . . . f e e e e e e e e e e f e . . . . .
+        . . . . . 6 f e e e e e e e e e e f 6 . . . . .
+        . . . . 6 7 7 6 e e e e e e e e e 6 7 6 . . . .
+        . . . 6 7 7 7 6 6 6 e e e e 6 6 6 7 7 7 6 . . .
+        . . 6 7 7 6 e e 6 7 7 7 7 7 7 7 6 6 7 7 7 6 . .
+        . . . 6 6 8 e e 7 7 6 8 8 6 7 7 8 8 6 6 6 . . .
+        . . . . . . e e 7 7 e e e e 6 7 e e . . . . . .
+        . . . . . . e e 6 e e e e e e 6 c e . . . . . .
+        . . . . . . e e f e e e e e e e c e . . . . . .
+        . . . . . . e e c e e e e e e e c e . . . . . .
+        . . . . . . e e c e e e e e e e f e . . . . . .
+        . . . . . . e e c e e e e e e e f e . . . . . .
+        . . . . . . e e e e e e e e e e f e . . . . . .
+        . . . . . . e e e e e e e e e e c e . . . . . .
+        . . . . . 6 e e e e e e e e e e c e 6 . . . . .
+        . . . . 6 7 7 6 e e e e e e e e e 6 7 6 . . . .
+        . . . 6 7 7 6 6 6 6 e e e e 6 7 6 6 7 7 6 . . .
+        . . 6 7 7 6 e e 7 7 7 7 7 7 7 7 6 6 7 7 7 6 . .
+        . . . 6 6 8 c e 7 7 6 8 8 6 7 7 8 8 6 6 6 . . .
+        . . . . . . c e 7 7 e e e e 6 7 e e . . . . . .
+        . . . . . . c e 6 e e e e e e 6 e e . . . . . .
+        . . . . . . c e e e e e e e e e e e . . . . . .
+        . . . . . . f c c e e e e e e c e e . . . . . .
+        . . . . . . f c c c e e e c e c c e . . . . . .
+        . . . . . . f c c e e e c c e c c c . . . . . .
+        . . . . . . f c c c e e c c e c c c . . . . . .
+        . . . . . . f c c c c c e c e e c c . . . . . .
+        . . . . . 6 f c c c c c c c c c c f 6 . . . . .
+        . . . . 6 7 7 6 c c c c c c c c c 6 7 6 . . . .
+        . . . 6 7 7 6 6 7 6 c c c c 6 7 6 6 7 7 6 . . .
+        . . 6 7 7 6 c c 7 7 7 7 7 7 7 7 6 6 7 7 7 6 . .
+        . . . 6 6 8 c c 7 7 6 8 8 6 7 7 8 8 6 6 6 . . .
+        . . . . . . c c 7 7 c c c c 6 7 c f . . . . . .
+        . . . . . . c c 6 c c c c c c 6 c f . . . . . .
+    `, SpriteKind.Enemy)
+    obsticleBottom.setPosition(scene.screenWidth(), 90)
+    obsticleBottom.vx = -25
+}
+
+game.onUpdateInterval(2000, function () {
+    if(Math.random() < 0.5) {
+        spawnTopTree()
+    } else {
+        spawnBottemTree()
+    }
+})    
+
+
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite: Sprite, otherSprite: Sprite) {
+    game.over()
+})
+
+
+
